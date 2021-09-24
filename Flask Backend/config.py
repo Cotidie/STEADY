7f1a3f0eb0b71384ed5acb9ecdc7963e4cf1121a
WonSeok <daily142857@gmail.com>
2021-09-24 21:47:23 +0900

    Add settings for MongoDB

--- Flask Backend/config.py
+++ Flask Backend/config.py
@@ -1,12 +1,20 @@
+from os.path import isfile
+
 """
  from_pyfile()로 읽어들일 Flask config 파일
   * 변수명은 반드시 대문자로 설정해야 한다.
   * 공통 접두사는 Flask에서 namespace로 활용한다.
 """
 
+# 사전검사
+if not isfile('db_host.0'):
+    raise Exception('db_host.0 파일이 존재하지 않습니다.')
+
 
 # MongoDB Settings
-MONGODB_URI = ''
+with open('db_host.0') as file:
+    MONGODB_HOST = file.readline()
+MONGODB_DB = 'miniter'
 
 # Custom Settings
 ADMINS = [
