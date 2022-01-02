9152c485433180fffa6b5b4d755d10f1d048db1a
Won Suk <daily142857@gmail.com>
2022-01-03 00:36:09 +0900

    Add basic drawing feature

--- Kotlin/07-DrawingApp/app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ Kotlin/07-DrawingApp/app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -1,23 +1,93 @@
 package com.example.drawingapp
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 
 class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     private var mDrawPath: CustomPath? = null
-    private var mBitmap: Bitmap? = null
+    private var mCanvasBitmap: Bitmap? = null
     private var mDrawPaint: Paint? = null
     private var mCanvasPaint: Paint? = null
     private var mBrushSize: Float = 0F
+    // View가 그려질 화면과 그 색상
+    private var canvas: Canvas? = null
     private var color = Color.BLACK
 
+    // Primary Constructor 이후에 실행
+    init {
+        setupDrawing()
+    }
 
-    inner class CustomPath : Path() {
+    private fun setupDrawing() {
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint = Paint()
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.BEVEL
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20F
+    }
 
+    // Path에 색상과 굵기 추가
+    inner class CustomPath(
+        var color: Int,
+        var brushThickness: Float
+    ) : Path()
+
+    // 액티비티 시작 시 함께 실행
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        // 초기화하지 않으면 Path는 계속 덧씌워진다 => 메모리 낭비
+        canvas?.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = Color.GREEN
+            canvas?.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    // 약 1픽셀 단위로 수없이 호출된다.
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when(event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+
+                // moveTo 설정 등을 초기화
+                mDrawPath!!.reset()
+                // lineTo의 시작점 제공
+                mDrawPath!!.moveTo(touchX!!, touchY!!)
+            }
+            MotionEvent.ACTION_UP -> {
+                // 선 초기화
+                // 아래 invalidate()에서 초기화되면서 화면에서 사라짐
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+            MotionEvent.ACTION_MOVE -> {
+                mDrawPath!!.lineTo(touchX!!, touchY!!)
+            }
+            else -> return false
+        }
+        // onDraw() 호출
+        invalidate()
+
+        return true
     }
 }
