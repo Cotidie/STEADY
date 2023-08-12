f1f5990c3a98fe86042713eb71d883932e83b92c
Cotidie <daily142857@gmail.com>
2023-08-13 00:47:07 +0900

    Leetcode 128: Longest Consecutive Sequence

--- algorithm/Leetcode/128/Longest Consecutive Sequence.cpp
+++ algorithm/Leetcode/128/Longest Consecutive Sequence.cpp
@@ -1,12 +1,80 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
+
+class Node {
+public:
+    Node() {};
+    Node(int val): val(val) {};
+
+    bool isRoot() { return next == nullptr; }
+
+    Node* next = nullptr;
+    int val = 0;
+    int size = 1;
+};
+
+class DisjointSet {
+public:
+    DisjointSet(vector<int>& nums) {
+        for (int num : nums) {
+            sets[num] = Node(num);
+        }
+    }
+
+    Node& Find(int val) {
+        Node& node = sets[val];
+
+        if (node.isRoot()) return node;
+
+        Node& root = Find(node.next->val);
+        node.next = &root;
+
+        return root;
+    }
+
+    bool Union(int a, int b) {
+        Node& aRoot = Find(a);
+        Node& bRoot = Find(b);
+
+        if (aRoot.val == bRoot.val) return false;
+
+        if (aRoot.size > bRoot.size) {
+            bRoot.next = &aRoot;
+            aRoot.size += bRoot.size;
+        } else {
+            aRoot.next = &bRoot;
+            bRoot.size += aRoot.size;
+        }
+
+        return true;
+    }
+
+private:
+    map<int, Node> sets;
+};
 
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        
+        if (nums.size() == 0) return 0;
+
+        map<int, bool> exists;
+        DisjointSet disjointSet(nums);
+
+        for (int num : nums) exists[num] = true;
+
+        int largestSize = 1;
+        for (int num : nums) {
+            if (exists.find(num-1) == exists.end()) continue;
+
+            disjointSet.Union(num, num-1);
+            largestSize = max(largestSize, disjointSet.Find(num).size);
+        }
+
+        return largestSize;
     }
 };
 
--- algorithm/Leetcode/128/README.md
+++ algorithm/Leetcode/128/README.md
@@ -2,22 +2,15 @@
 [Longest Consecutive Sequence](https://leetcode.com/problems/longest-consecutive-sequence/)
 
 ## Topic
-- Tree
-- Recursion
+- Union Find
 
 ## Approach
-![img](https://i.imgur.com/Qtgiiar.png)
-
-Those two are the keys for the approach.
-- The first element of pre-order traverse is always the root node.
-- The left side and the right side of the root node in in-order traverse is its left sub-tree and its right sub-tree
-
-1. First, set the first element of pre-order as current root node
-  - then take it out from the array
-2. Find the index of the root node from in-order array. 
-  - left side becomes its left sub-tree
-  - right side becomes its right sub-tree
-4. Repeat 1 & 2  on the left sub-tree and the right sub-tree
-  - until no element is left in either pre-ordere array or in-order array
-
-## 비고/남길말
+1. Build a hashmap and a disjoint set
+    - hashmap: to check its existence
+    - disjoint set: to merge consequtive numbers
+2. Iterate the array and check if the left(-1) value exists
+3. If exists, union two numbers
+    - memorize the biggest size
+4. return the biggest size
+   
+## Note
